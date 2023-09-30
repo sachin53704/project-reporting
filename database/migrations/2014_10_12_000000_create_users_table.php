1ff68bf0a3ae9ab677dfb9ecceb5aa8546b75ea7
coreoceantesting <coreoceantesting@gmail.com>
2023-09-30 19:01:39 +0530

    Add log package

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('ip')->nullable();
+            $table->string('login_session')->nullable();
+            $table->dateTime('login_time')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
